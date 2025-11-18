9409fceced6db3ccbb705024caa13ede83039812
John Doe <izan95291@gmail.com>
2025-11-17 23:37:55 -0500

    finally

--- modules/DevPackages/fluid.lua
+++
@@ -1,6 +0,0 @@
-local REQUIRED_MODULE = require(script.Parent._Index["ffrostfall_fluid@0.6.4"]["fluid"])
-export type UsedAs<T> = REQUIRED_MODULE.UsedAs<T>
-export type Source<T> = REQUIRED_MODULE.Source<T>
-export type Action = REQUIRED_MODULE.Action 
-export type PropertiesFor<T> = REQUIRED_MODULE.PropertiesFor<T>
-return REQUIRED_MODULE
--- modules/DevPackages/jecs-utils.lua
+++
@@ -1,1 +0,0 @@
-return require(script.Parent._Index["pepeeltoro41_jecs-utils@1.1.1"]["jecs-utils"])
